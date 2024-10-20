7c43a919650c281e82bfdc2929e9c1ae9fc9dbe5
Bashamega <adambashaahmednaji@gmail.com>
2024-10-20 12:31:40 +0300

    feat: set command

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-const { spawn } = require('node:child_process');
+const { exec } = require('child_process');
 class Lunar {
     constructor(){
-        this.basUrl = 'http://localhost:8000';
+        this.baseUrl = 'http://localhost:8000';
     }
     connection(connectionURL){
-        this.basUrl = connectionURL
+        this.baseUrl = connectionURL;
+    }
+    set(key, value, ttl){
+        try {
+            const command = `
+                lunar
+                set ${key} ${value} ${ttl ? ttl : ''}
+            `
+            exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    return `exec error: ${error}`;
+                }
+                return stdout
+            });
+        } catch (error) {
+            return error
+        }
     }
 }
