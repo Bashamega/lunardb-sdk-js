8c971a0e977612252d74564aa538afd8847485df
Bashamega <adambashaahmednaji@gmail.com>
2024-10-22 07:59:53 +0300

    feat: get key

--- src/index.js
+++ src/index.js
@@ -22,5 +22,21 @@ class Lunar {
             return error
         }
     }
+    get(key){
+        try {
+            const command = `
+                lunar
+                get ${key}
+            `
+            exec(command, (error, stdout, stderr) => {
+                if (error) {
+                    return `exec error: ${error}`;
+                }
+                return stdout
+            });
+        } catch (error) {
+            return error
+        }
+    }
 }
 module.exports = Lunar;
